fe0cd337db9182383195b5f52a2f0bf61b4b0b82
Yee Yong <168053370+ChuahYeeYong@users.noreply.github.com>
2025-04-21 15:35:34 +0800

    Removed lazy loading and added eleventh section
    Lazy loading causing positioning of seventh section cup not centered sometimes

--- script.js
+++ script.js
@@ -656,9 +656,7 @@ $(function () {
     trigger: ".touching-hands",
     start: "top top",
     end: "+=1300",
-    // pin: true,
     scrub: true,
-    markers: true, // optional: remove in production
   });
   function moveHands(id, xAmount, yAmount, rotation) {
     gsap.from(id, {
@@ -705,7 +703,7 @@ $(function () {
       y: "-40vh",
       scrollTrigger: {
         trigger: ".exploding-lady",
-        start: "top bottom",
+        start: "center bottom",
         end: "bottom bottom",
         scrub: true,
       },
@@ -715,3 +713,52 @@ $(function () {
   chilliMoveY();
 });
 // TENTH SECTION ANIMATIONS END
+// ELEVENTH SECTION ANIMATIONS
+$(function () {
+  gsap.to('.scroll', {
+    autoAlpha:0,
+    duration:0.2,
+    scrollTrigger: {
+        trigger:'.teh-tarik-malaysian',
+        start:'top top',
+        end:'top top-=1',
+        toggleActions: "play none reverse none"
+    }
+})
+
+const root = document.querySelector('.teh-tarik-malaysian')
+
+// Get the actual text content
+const textPath = root.querySelector('#textpath')
+const text = textPath.textContent.trim()
+
+// Calculate text width using canvas
+const textPathLength = getTextWidth(text) * 1.25
+
+// Calculate final offset percentage
+const pathLength = root.querySelector('#path').getTotalLength()
+const finalOffset = -(textPathLength * 100 / pathLength)
+
+gsap.to(textPath, {
+    attr: { startOffset: finalOffset + "0%" }, // Here we are targeting an attribute value, not a CSS property
+    ease:'none', // Linear movement
+    scrollTrigger: {
+        trigger: '.teh-tarik-malaysian',
+        start: 'top top',
+        end: '+=2000',
+        pin: '.teh-tarik-malaysian',
+        scrub: true // Progresses with the scroll
+    }
+}) 
+
+// UTIL METHOD
+function getTextWidth(text) {
+    const canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement("canvas"))
+    const context = canvas.getContext("2d")
+    const computedStyle = window.getComputedStyle(document.querySelector('#textpath'))
+    context.font = computedStyle.font
+    
+    return context.measureText(text).width
+}
+});
+// ELEVENTH SECTION ANIMATIONS END
